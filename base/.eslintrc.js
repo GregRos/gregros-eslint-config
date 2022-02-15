2c9e907fa07656ab83363a90ee861b0827686457
GregRos <life.gregr@gmail.com>
2022-02-15 22:04:50 +0200

    fixed version

--- base/.eslintrc.js
+++ base/.eslintrc.js
@@ -2,7 +2,7 @@ const path = require("path");
 module.exports = {
     root: true,
     extends: [
-        "@reflectiz/eslint-config"
+        "@gregros/eslint-config"
     ],
     parserOptions: {
         project: path.join(__dirname, "tsconfig.json")
