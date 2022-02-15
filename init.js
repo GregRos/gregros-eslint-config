c79b56b77cdc77335877e4cb7666ccc5d962437d
GregRos <life.gregr@gmail.com>
2022-02-15 22:03:34 +0200

    use yarn too

--- init.js
+++ init.js
@@ -4,20 +4,21 @@ const {join} = require("path")
 const {cp, echo, exec, set, config} = require("shelljs");
 config.globOptions.dot = true;
 echo("Installing peer dependencies...");
-const cmd = process.argv0 === "npm" ? "npm install --save-dev" : "yarn add --dev";
-
+const cmdAdd = process.argv0 === "npm" ? "npm install --save-dev" : "yarn add --dev";
+const cmdCheck = process.argv0 === "npm" ? "npm list" : "yarn info"
 const packageJson = require("./package.json");
 const peers = packageJson.peerDependencies;
 const deps = Object.entries(peers)
+
 for (const [k, v] of deps) {
-    const result = exec(`npm list ${k}`);
+    const result = exec(`${cmdCheck} ${k}`);
     // If the command succeeded, skip to the next one.
     if (result.code === 0) {
         echo(`* Peer dependency ${k} exists.`)
         continue;
     }
     echo(`* Installing peer dependency ${k}...`)
-    exec(`${cmd} ${k}@${v}`)
+    exec(`${cmdAdd} ${k}@${v}`)
 }
 echo("Copying files to cwd...")
 const filesDir = join(__dirname, "base", "*");
