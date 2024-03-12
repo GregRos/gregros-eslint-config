891925f270f09ec8249b66575a44b61492e7b358
GregRos <life.gregr@gmail.com>
2024-03-12 20:42:54 +0200

    Updated dependencies, removed peer because it's annoying

--- init.js
+++ init.js
@@ -8,6 +8,9 @@ const yarn = {
     name: "yarn",
     install(...what) {
         return exec(`yarn add --dev ${what.join(" ")}`).code === 0;
+    },
+    uninstall(...what) {
+        return exec(`yarn remove --dev ${what.join(" ")}`).code === 0;
     }
 };
 
@@ -15,16 +18,23 @@ const npm = {
     name: "npm",
     install(...what) {
         return exec(`npm install --save-dev ${what.join(" ")}`).code === 0;
+    },
+    uninstall(...what) {
+        return exec(`npm uninstall --save-dev ${what.join(" ")}`).code === 0;
     }
 };
 
 const pkg = test("-f", "yarn.lock") ? yarn : npm;
 echo(`Installing peer dependencies. Using ${pkg.name}.`);
 const myPackageJson = require("./package.json");
-const peers = myPackageJson.peerDependencies || {};
+const peers = myPackageJson.autoInstall || {};
+const names = Object.keys(peers);
+const list = names.join(" ");
+console.log(`Uninstalling: ${list}`)
+pkg.uninstall(...names);
 const packages = Object.entries(peers).map(([k, v]) => `${k}@${v}`);
 console.log(`
-Will be installed:
+Installing:
 ${packages.map(x => `* ${x}`).join("\n")}
 `.trim());
 pkg.install(...packages);
